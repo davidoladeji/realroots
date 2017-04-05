2b52ccbb3be4d7af8382b5767b7d44c1d3ed0e53
David Oladeji <david.oladeji.daveola@gmail.com>
2017-04-05 21:47:48 +0100

    Program header message added

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    /**
+     * A program that calcualtes the real roots of a quadratic equation
+     */
+    double a, b, c, determinant, root1,root2, realPart, imaginaryPart;
+
+    printf("Enter coefficients a, b and c: ");
+    //1 before the regular
+    scanf("%lf %lf %lf",&a, &b, &c);
+
+    determinant = b*b-4*a*c;
+
+    // condition for real and different roots
+    if (determinant > 0)
+    {
+        // sqrt() function returns square root
+        root1 = (-b+sqrt(determinant))/(2*a);
+        root2 = (-b-sqrt(determinant))/(2*a);
+
+        printf("root1 = %.2lf and root2 = %.2lf",root1 , root2);
+    }
+
+        //condition for real and equal roots
+    else if (determinant == 0)
+    {
+        root1 = root2 = -b/(2*a);
+
+        printf("root1 = root2 = %.2lf;", root1);
+    }
+
+        // if roots are not real 
+    else
+    {
+        realPart = -b/(2*a);
+        imaginaryPart = sqrt(-determinant)/(2*a);
+        printf("root1 = %.2lf+%.2lfi and root2 = %.2f-%.2fi", realPart, imaginaryPart, realPart, imaginaryPart);
+    }
+
+    return 0;
+}   
